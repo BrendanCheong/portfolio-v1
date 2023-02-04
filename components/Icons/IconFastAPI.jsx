b6b0f38a750dc5965a512ce9958f1a5e968c957e
Brendan Cheong <brendan_cej@u.nus.edu>
2023-02-04 17:21:03 +0800

    feat: added icons svg jsx components

---
+++ components/Icons/IconFastAPI.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function IconFastAPI() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='50px'
+			height='50px'
+			fill='#000000'
+			viewBox='0 0 154 154'
+		>
+			<circle cx='77' cy='77' r='77' fill='#05998b'></circle>
+			<path
+				fill='#fff'
+				d='M81.375 18.667l-38.75 70H77.5l-3.875 46.666 38.75-70H77.5z'
+			></path>
+		</svg>
+	);
+}
+
+export default IconFastAPI;
