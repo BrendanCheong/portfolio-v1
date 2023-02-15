399aa04e8781ce40217ba5e5469cddd213b05641
Brendan Cheong <brendan_cej@u.nus.edu>
2023-02-16 00:42:52 +0800

    feat: experiment with adding SVGs instead of images

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as d3 from 'd3';
 import Container from '../components/Container';
 import { useD3 } from '../hooks/useD3';
 import Heading from './Heading';
+import { range } from '../lib/functions';
 
 const Skills = () => {
 	// create a ref to attach to the DOM
 	// this D3 layout will create divs with background images that will be used as the skills icons
-	// each div will have a class of 'skill-icon' and an id of the skill name
+	// each div will have a class of 'skill-bubble' and an id of the skill name
 	// each div can collide with each other using elastic collision
-
 	const skills = [
 		'React',
 		'Next.js',
@@ -30,6 +30,18 @@ const Skills = () => {
 		'FastAPI',
 		'TailwindCSS',
 	];
+
+	const nodes = d3.range(skills.length + 1).map(() => {
+		return { radius: 35 };
+	});
+	const [D3nodes, setD3Nodes] = useState(nodes);
+	const [toggle, setToggle] = useState(false);
+
+	// create a function that setsD3Nodes but with one less node
+	const removeNode = () => {
+		d3.selectAll('#skill-bubble').remove();
+		setD3Nodes(D3nodes.slice(0, -1));
+	};
 
 	const collide = (node) => {
 		const r = node.radius + 1;
@@ -59,68 +71,68 @@ const Skills = () => {
 	const height = 300;
 
 	const ref = useD3((svg) => {
-		const nodes = d3.range(skills.length + 1).map(() => {
-			return { radius: 35 };
-		});
-		const root = nodes[0];
+		// const nodes = d3.range(skills.length + 1).map(() => {
+		// 	return { radius: 35 };
+		// });
+		const root = D3nodes[0];
 
 		root.radius = 0;
 		root.fixed = true;
 
+		console.log('D3 render');
+
 		const force = d3.layout
 			.force()
-			.gravity(0.15)
-			.friction(0.9)
-			.charge((_d, i) => {
+			.gravity(0.05)
+			.friction(0.2)
+			.charge((_data, i) => {
 				return i ? 0 : -2000;
 			})
-			.nodes(nodes)
+			.nodes(D3nodes)
 			.size([width, height])
 			.on('tick', () => {
-				const q = d3.geom.quadtree(nodes);
-				let i = 0;
-				const n = nodes.length;
+				const q = d3.geom.quadtree(D3nodes);
+				const n = D3nodes.length;
 
-				while (++i < n) q.visit(collide(nodes[i]));
+				range(0, n).forEach((_element, i) => {
+					q.visit(collide(D3nodes[i]));
+				});
 
 				svg
-					.selectAll('#skill-icon')
-					.style('left', (d) => {
-						return `${d.x}px`;
+					.selectAll('#skill-bubble')
+					.style('left', (data) => {
+						return `${data.x}px`;
 					})
-					.style('top', (d) => {
-						return `${d.y}px`;
+					.style('top', (data) => {
+						return `${data.y}px`;
 					});
 			})
 			.start();
 
 		svg = d3
 			.select('#D3')
-			// .append('section')
-			.attr('viewBox', '0 0 ' + width + ' ' + height)
 			.attr('preserveAspectRatio', 'xMinYMin')
-			.attr('style', () => {
-				return `position: relative`;
-			});
 
 		svg
 			.selectAll('#D3')
-			.data(nodes.slice(1))
+			.data(D3nodes.slice(1))
 			.enter()
 			.append('div')
-			.attr('id', 'skill-icon')
-			.style('height', '63px')
-			.style('width', '63px')
-			.style('border-radius', '100%')
-			.style(
-				'background-image',
-				'url("https://bookface-images.s3.amazonaws.com/small_logos/d0e24465d91469fa05da337659e25131f5295e3d.png")'
-			)
-			.style('background-size', 'cover')
-			.style('background-position', 'center center')
-			.style('background-repeat', 'no-repeat')
-			.style('transform', 'translate(-50%, -50%)')
-			.style('position', 'absolute');
+			.attr('id', 'skill-bubble')
+			.attr('class', 'h-16 w-16 rounded-full translate-x-[-50%] translate-y-[-50%] absolute hover:text-primary-light/100 text-primary-light/0 hover:text-opacity-100 z-0 hover:z-50')
+			.html(`<svg
+			xmlns='http://www.w3.org/2000/svg'
+			viewBox='0 0 30 30'
+			width='63px'
+			height='63px'
+			class='text-white-dark hover:text-primary-light fill-current transition-[opacity_.5s,color_.5s]'
+		>
+			<path d='M25.428,3.333C25.238,3.121,24.967,3,24.683,3H5.317C5.033,3,4.762,3.121,4.572,3.333c-0.19,0.212-0.28,0.495-0.249,0.777 l2.202,19.823c0.044,0.403,0.329,0.74,0.719,0.851l7.48,2.137c0.09,0.026,0.183,0.039,0.275,0.039s0.185-0.013,0.275-0.039 l7.48-2.137c0.39-0.111,0.674-0.448,0.719-0.851L25.676,4.11C25.708,3.828,25.618,3.545,25.428,3.333z M20.629,10.43h-8.93 l0.212,2.542h8.503l-0.638,7.51L15.003,22l-0.047-0.015l-4.72-1.505L9.978,17.42h2.312l0.101,1.189l2.637,0.581l2.591-0.582 l0.275-3.213h-8.09L9.178,8h11.659L20.629,10.43z' />
+		</svg>`)
+		.append('i')
+		.attr('class', 'font-poppins text-sm')
+		.attr('id', 'skill-name')
+		.html('diverse');
 
 		svg.on('mousemove', function () {
 			const p1 = d3.mouse(this);
@@ -134,7 +146,7 @@ const Skills = () => {
 		<Container>
 			<section className='flex flex-row items-start w-full h-full pb-10'>
 				<div className='basis-5/12 md:block flex-grow-0 flex-shrink-0 order-last hidden ml-5'>
-					<div className='h-full' ref={ref} id='D3'></div>
+					<div className='relative h-full' ref={ref} id='D3'></div>
 				</div>
 				<div>
 					<div className='pb-5 space-y-2'>
@@ -166,6 +178,9 @@ const Skills = () => {
 						</p>
 					</div>
 				</div>
+				<button className='w-10 h-10 bg-red-500 rounded-md' onClick={() => removeNode()}>
+					Test
+				</button>
 			</section>
 		</Container>
 	);
